9f6d020c7650ae8fdb1cfe3c8f8df32a53319858
Ian Hangartner <icrashstuff@outlook.com>
2025-12-13 14:28:37 -0900

    Add some functions related to using the default timer as a realtime clock

--- main.c
+++ main.c
@@ -20,14 +20,58 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
-int main() {
+#include <sys/time.h>
+#include <time.h>
+
+#define arraysizeof(array) (sizeof(array) / sizeof(array[0]))
+
+void set_unix_time_us(uint64_t microseconds_seconds_since_1970)
+{
+    uint64_t new_val = microseconds_seconds_since_1970;
+    timer_hw_t* timer = PICO_DEFAULT_TIMER_INSTANCE();
+
+    uint32_t lo = new_val & 0xFFFFFFFFu;
+    uint32_t hi = new_val >> 32u;
+
+    timer->timelw = lo;
+    timer->timehw = hi;
+}
+
+void set_unix_time(time_t seconds_since_1970) { set_unix_time_us(seconds_since_1970 * 1000000); }
+
+time_t get_unix_time() { return time_us_64() / 1000000ul; }
+
+struct tm get_utc_time()
+{
+    struct tm utc;
+    time_t t = get_unix_time();
+
+    gmtime_r(&t, &utc);
+
+    return utc;
+}
+
+int main()
+{
     stdio_init_all();
-    while(true)
+
+    while (true)
     {
-        printf("Hello, world!\n");
+        const struct tm utc = get_utc_time();
+        struct tm local = utc;
+
+        local.tm_hour -= 9;
+
+        // Fix local
+        mktime(&local);
+
+        printf("%s\n", asctime(&local));
+
         sleep_ms(1000);
     }
     return 0;
